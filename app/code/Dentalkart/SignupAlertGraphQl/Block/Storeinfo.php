241e7ab0dc5d9bed74ee53b168c2b9345223a201
bhaskar15435 <bhaskar15435@gmail.com>
2019-07-08 17:51:26 +0530

    just delete

---
+++ app/code/Dentalkart/SignupAlertGraphQl/Block/Storeinfo.php
@@ -0,0 +1,24 @@
+<?php
+namespace Dentalkart\SignupAlertGraphQl\Block;
+
+class Storeinfo extends \Magento\Framework\View\Element\Template
+{
+
+  public $storeManager;
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        array $data = []
+    ) {
+        $this->storeManager = $storeManager;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return Store
+     */
+    public function getStore()
+    {
+        return $this->storeManager->getStore();
+    }
+  }
---
+++ app/code/Dentalkart/SignupAlertGraphQl/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'Dentalkart_SignupAlertGraphQL',
+	__DIR__
+ );
